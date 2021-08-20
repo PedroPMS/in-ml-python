fa2fa39fbac7d66d3e4a194755bc0543e90ef828
Pedro Paulo Silva <p.paulo0512@gmail.com>
2021-08-20 12:03:48 -0300

    criando a arvore de decisao

--- init.py
+++ init.py
@@ -1,11 +1,21 @@
-from sklearn.tree import DecisionTreeClassifier
-import csv
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
 import pandas as pd
 import tratarDados
 
 tratarDados.tratarDados()
 
 dados = pd.read_csv('./dermatology.data')
+dados_treino, dados_teste, resultados_treino, resultados_teste = train_test_split(
+    dados.drop('result', axis=1), dados['result'], test_size=0.3)
 
-dados.head()
-dados.info()
+arvore = DecisionTreeClassifier()
+arvore = arvore.fit(dados_treino, resultados_treino)
+
+for feature, importancia in zip(dados.columns, arvore.feature_importances_):
+    print("{}: {}".format(feature, importancia))
+
+resultadoPredicao = arvore.predict(dados_teste)
+print(metrics.classification_report(resultados_teste, resultadoPredicao))
+plot_tree(arvore)
--- tratarDados.py
+++ tratarDados.py
@@ -1,10 +1,21 @@
 def tratarDados():
-    with open('./dermatology.names', 'r+') as file:
-        header = file.readline()
+    with open('./dermatology.names', 'r+') as cabecalho:
+        header = cabecalho.readline()
+    cabecalho.close()
 
-    with open('./dermatology.data', 'r+') as f:
-        firstLine = f.readline()
+    with open('./dermatology.data', 'r+') as dados:
+        contents = dados.read()
+        retirandoInterrogacao = contents.replace('?', '0')
+    dados.close()
+
+    with open("./dermatology.data", "w") as dados:
+        dados.write(retirandoInterrogacao)
+    dados.close()
+
+    with open('./dermatology.data', 'r+') as dados:
+        firstLine = dados.readline()
         if (firstLine[0].isnumeric()):
-            content = f.read()
-            f.seek(0, 0)
-            f.write(header.rstrip('\r\n') + '\n' + content)
+            content = dados.read()
+            dados.seek(0, 0)
+            dados.write(header.rstrip('\r\n') + '\n' + content)
+    dados.close()
