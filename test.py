978cfa3c41473014183b3105f2b74ff77aa24d69
Pedro Paulo Silva <p.paulo0512@gmail.com>
2021-08-23 20:03:02 -0300

    iniciando implementaçao do kmeans

---
+++ test.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set()  # for plot styling
+import numpy as np
+from sklearn.datasets import make_blobs
+
+X, y_true = make_blobs(n_samples=300,
+                       centers=4,
+                       cluster_std=0.60,
+                       random_state=0)
+print(X)
+plt.scatter(X[:, 0], X[:, 1], s=50)
+plt.show()
